f178cd49e7d713da59becd4526aa57318613254c
Angel D <angel.can.work@gmail.com>
2020-11-11 00:18:17 -0500

    Autoscroll for chat.

--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 // import * as AWS from 'aws-sdk';
 import { LexRuntime } from 'aws-sdk';
 import { environment } from 'src/environments/environment';
@@ -17,6 +17,7 @@ export class ChatbotComponent implements AfterViewInit {
   lex: LexRuntime;
   messages: Message[];
   lexState: string;
+  @ViewChild('scroll', { static: true }) scroll: any;
 
   constructor() {
     this.lex = new LexRuntime({
@@ -28,24 +29,28 @@ export class ChatbotComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-  }
+    this.scroll.nativeElement.scrollTop = this.scroll.nativeElement.scrollHeight;
+}
 
-  postLexText(userInput) {
-    const params = {
-      botAlias: 'schedule',
-      botName: 'ScheduleAnAppointment',
-      inputText: 'Testing',
-      userId: 'User',
-    };
-    params.inputText = userInput;
-    this.lex.postText(params, (err, data) => {
-      if (err) {
-        console.log(err, err.stack); // an error occurred
-      } else {
-        this.lexState = data.message;
-      }
-      this.messages.push(new Message(userInput, 'User'));
-      this.messages.push(new Message(this.lexState, 'Bot'));
-    });
-  }
+postLexText(userInput) {
+  const params = {
+    botAlias: 'schedule',
+    botName: 'ScheduleAnAppointment',
+    inputText: 'Testing',
+    userId: 'User',
+  };
+  params.inputText = userInput;
+  this.lex.postText(params, (err, data) => {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+    } else {
+      this.lexState = data.message;
+    }
+    this.messages.push(new Message(userInput, 'User'));
+    this.messages.push(new Message(this.lexState, 'Bot'));
+    setTimeout(() => {
+      this.scroll.nativeElement.scrollTo(0, this.scroll.nativeElement.scrollHeight);
+    }, 0);
+  });
+}
 }
